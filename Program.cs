67e17066c213db78dd9b6ad42d89b852035e0151
Vasuki <vasuki.k@gmail.com>
2018-05-09 11:44:15 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Name");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night ??");
+            int hourofSleep = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Hello " + name);
+            if(hourofSleep >=8)
+            {
+                Console.WriteLine("You Have Enough Sleep");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
+        }
+    }
+}
